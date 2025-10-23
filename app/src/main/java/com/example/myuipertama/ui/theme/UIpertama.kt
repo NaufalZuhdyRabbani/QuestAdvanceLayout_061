40d67940996fc568f0a124417c50c7a0135681ca
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2025-10-23 11:42:41 +0700

    composable

---
+++ app/src/main/java/com/example/myuipertama/ui/theme/UIpertama.kt
@@ -0,0 +1,11 @@
+package com.example.myuipertama.ui.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ActivitasPertama(modifier : Modifier){
+    Coloumn { modifier = Modifier.padding(top=100.dp)
+        .fillMaxSize(),
+    horizontalAlignment = Aligment.CenterHorizontally
+        )}
+}
