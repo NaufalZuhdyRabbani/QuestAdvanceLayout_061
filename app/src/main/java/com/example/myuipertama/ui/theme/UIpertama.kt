cfde861a85eb1fcbc195af898f835547c22a971f
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2025-10-23 13:01:17 +0700

    membetulkan typo

--- app/src/main/java/com/example/myuipertama/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/myuipertama/ui/theme/UIpertama.kt
@@ -98,7 +98,7 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                 stringResource(R.string.copy),
                 modifier = Modifier
                     .align (Alignment.BottomCenter)
-                    .padding(bottom = 50.dp)
+                    .padding(bottom = 50.dp)bkas
             )
         }
     }
