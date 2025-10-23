86014ae8ad23fa6420d649c184b525a1555a1f23
NaufalZuhdyRabbani <naufalkzrz@gmail.com>
2025-10-23 12:38:40 +0700

    bukan copy paste mas ini lupa commit demi allah menambahkan foto membenarkan error

--- app/src/main/java/com/example/myuipertama/ui/theme/UIpertama.kt
+++ app/src/main/java/com/example/myuipertama/ui/theme/UIpertama.kt
@@ -1,19 +1,94 @@
 package com.example.myuipertama.ui.theme
 
+import android.R.attr.fontFamily
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.createFontFamilyResolver
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myuipertama.R
 
 @Composable
-fun ActivitasPertama(modifier : Modifier){
-    Column ( modifier = Modifier.padding(top=100.dp)
-        .fillMaxSize(),
-    horizontalAignment = Aligment.CenterHorizontally
-        ){
-            Text(stringResource(R.string.prodi),
-                fontSize = 35.sp,
-                fontWeight = FontWeight.Bold)
+fun AktivitasPertama(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(R.string.prodi),
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
 
+        Text(
+            text = stringResource(R.string.univ),
+            fontSize = 22.sp
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
+        ) {
+            Row(
+                modifier = Modifier.padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val gambar = painterResource(R.drawable.logo_umy)
+                Image(
+                    painter = gambar,
+                    contentDescription = "Logo UMY",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(5.dp)
+                )
+
+                Spacer(modifier = Modifier.width(30.dp))
+
+                Column {
+                    Text(
+                        text = stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(5.dp)
+                    )
+
+                    Text(
+                        text = stringResource(R.string.alamat),
+                        fontSize = 20.sp,
+                        color = Color.Yellow,
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                }
+            }
         }
+    }
 }
+
